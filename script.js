d69b4be4a467c02a6659e0aeccbb99f5dfe56b86
Stewart Jester <jesterstewart@gmail.com>
2020-06-05 05:08:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+function getRepos() {
+  
+  let userName = $("#js-user").val();
+  
+  fetch(`https://api.github.com/users/` + userName + `/repos` 
+)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+  // console.log(address);
+}
+
+
+
+function displayResults(results) {
+  // console.log(responseJson.message);
+  
+  //replace the existing image with the new one
+  for (let i = 0; i < results.length; i++){
+    // $('.results').empty();
+    $('#results-list').append(
+    `<li><strong>Name:</strong> ${results[i].name}</li>
+     <p><a href="${results[i].url}">Repo</a>
+     </p> `
+  );
+  
+  }
+
+  //display the results section
+  $('#results').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    $( "#results-list" ).empty();
+    getRepos();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
